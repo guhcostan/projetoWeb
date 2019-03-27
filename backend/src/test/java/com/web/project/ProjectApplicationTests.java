e394c536f4dcd6117049837ecf147a5b1c510e45
Gustavo Costa <gustavo.neves.lemaf@gmail.com>
2019-03-27 09:04:58 -0300

    corrigido problema com maven

--- backend/src/test/java/com/web/project/ProjectApplicationTests.java
+++ backend/src/test/java/com/web/project/ProjectApplicationTests.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) ${project.inceptionYear} Gustavo Costa Neves (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.web.project;
 
 import org.junit.Test;
@@ -5,13 +20,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjectApplicationTests {
 
 	@Test
-	public String contextLoads() {
-		return "Sucesso";
+	public void contextLoads() {
+		String s = "sucesso";
+		assertEquals(s, "sucesso");
 	}
 
 }
