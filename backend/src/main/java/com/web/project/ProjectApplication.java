669ca6febccdcc5192be6a944ea93db455eeb7d7
Gustavo Costa <gustavo.neves.lemaf@gmail.com>
2019-03-27 08:36:26 -0300

    retirando codesmell

--- backend/src/main/java/com/web/project/ProjectApplication.java
+++ backend/src/main/java/com/web/project/ProjectApplication.java
@@ -3,12 +3,8 @@ package com.web.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.web.project")
@@ -16,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @EnableScheduling
 public class ProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
 
 }
