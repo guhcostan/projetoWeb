9f42d342cdb10bba7f763aabf77c26a2acf110df
Gustavo Costa Neves <guhcostan@gmail.com>
2019-04-05 18:52:54 -0300

    Update ProjectApplicationTests.java

--- backend/src/test/java/com/web/project/ProjectApplicationTests.java
+++ backend/src/test/java/com/web/project/ProjectApplicationTests.java
@@ -16,6 +16,7 @@
 package com.web.project;
 
 import org.junit.runner.RunWith;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
