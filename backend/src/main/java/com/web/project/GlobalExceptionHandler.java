568feac5494c9b9af325aecc5386d6a68652a1fe
Gustavo Costa <gustavo.neves.lemaf@gmail.com>
2019-04-17 18:43:50 -0300

    tentando corrigir checkstyle

--- backend/src/main/java/com/web/project/GlobalExceptionHandler.java
+++ backend/src/main/java/com/web/project/GlobalExceptionHandler.java
@@ -37,8 +37,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@Override
-	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
-															 HttpStatus status, WebRequest request) {
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex,
+															 Object body, HttpHeaders headers, HttpStatus status,
+															 WebRequest request) {
 
 		logger.error(ex.getLocalizedMessage(), ex);
 
