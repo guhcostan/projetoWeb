fdf6b26ddb0d4fcf50b7446734be4e3626ae9190
gureis <gureis4@gmail.com>
2019-05-12 00:41:13 -0300

    closes #10

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,97 +1,66 @@
 <template>
-    <el-row class="tac">
-    <el-col :span="12">
-        <h5>Default colors</h5>
-        <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose">
-        <el-submenu index="1">
-            <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>Navigator One</span>
-            </template>
-            <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item one</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-            <template slot="title">item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-            </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span>Navigator Two</span>
+    <el-menu
+        background-color="#112A37"
+        text-color="#4F5B68"
+        active-text-color="#63C3A8"
+        default-active="0"
+        :router="true">
+        <el-row class="logo" type="flex" justify="center" align="middle">
+            <el-col :span="19">
+                <img src="../assets/logo_GA.png" alt="Logo Gestão de Atividades">
+            </el-col>
+        </el-row>
+        <el-menu-item class="sidebar-item" v-for="item in sidebarItems" :key="item.id" :index="item.id.toString()" :route="item.urlRoute">
+            <SidebarItem :urlIcon="item.src" :title="item.title"></SidebarItem>
         </el-menu-item>
-        <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span>Navigator Four</span>
-        </el-menu-item>
-        </el-menu>
-    </el-col>
-    <el-col :span="12">
-        <h5>Custom colors</h5>
-        <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b">
-        <el-submenu index="1">
-            <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>Navigator One</span>
-            </template>
-            <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item one</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-            <template slot="title">item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-            </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span>Navigator Four</span>
-        </el-menu-item>
-        </el-menu>
-    </el-col>
-    </el-row>
+    </el-menu>
 </template>
 
 <script>
+    import SidebarItem from '@/components/SidebarItem.vue'
+
     export default {
         name: 'sideBar',
-        methods: {
-        handleOpen(key, keyPath) {
-            console.log(key, keyPath);
+        data() {
+            return {
+                sidebarItems: [
+                    { id: 0, src: "home-cinza.png", title: "Home", urlRoute: "/dashboard" },
+                    { id: 1, src: "user-cinza.png", title: "Alunos", urlRoute: "/dashboard/alunos" },
+                    { id: 2, src: "folder-cinza.png", title: "Projetos", urlRoute: "/dashboard/projetos" },
+                    { id: 3, src: "lab-cinza.png", title: "Grupo de Pesquisa", urlRoute: "/dashboard/pesquisa" },
+                    { id: 4, src: "document-cinza.png", title: "Publicações", urlRoute: "/dashboard/publicacoes" },
+                    { id: 5, src: "edit-cinza.png", title: "Disciplinas", urlRoute: "/dashboard/disciplinas" }
+                ]
+            }
         },
-        handleClose(key, keyPath) {
-            console.log(key, keyPath);
-        }
+        beforeUpdate() {
+
+        },
+        components: {
+            SidebarItem
         }
     }
 </script>
+
+<style lang="scss">
+    .logo {
+        .el-col {
+            text-align: center;
+            border-bottom: 1px solid #E4E6E9;
+            padding: 2rem 0 2rem 0;
+            height: 15vh;
+            img {
+                width: 6rem; 
+            }
+        }
+    }    
+    .sidebar-item {
+        height: auto;
+    }
+    .is-active {
+        border-left: 3px solid #63C3A8;
+        img {
+            filter: hue-rotate(304deg) saturate(2.5) brightness(195%);
+        }
+    }
+</style>
---
+++ frontend/src/components/SidebarItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-row type="flex" justify="center">
+        <el-col>
+            <img class="sidebar-icon" :src="require(`@/assets/icons/${urlIcon}`)"/>
+            <p class="title">{{title}}</p>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+    name: 'sidebarItem',
+    props: {
+        urlIcon: String,
+        title: String,
+        urlRoute: String
+    }    
+}
+</script>
+
+<style lang="scss">
+    .sidebar-item {
+        text-align: center;
+        .sidebar-icon {
+            padding-top: 1rem;
+            width: 4rem;
+            height: 4rem;
+        }
+        .title {
+            border-bottom: 1px solid #E4E6E9;
+            margin: 0;
+        }
+    }
+</style>
