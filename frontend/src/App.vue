b8f4ba8dece08dd9cb43f97a6061ed5da6eee16a
gureis <33041939+gureis@users.noreply.github.com>
2019-05-16 00:43:46 -0300

    Revert "Revert "Sidebar""

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,38 @@
-<template lang="pug">
-  #app
-    div
-      AppHeader
-      .view
-        router-view
-      AppFooter
+<template>
+  <div id="app">
+    <div v-if="!login">
+      <AppHeader></AppHeader>
+      <div class="view">
+        <router-view></router-view>
+      </div>
+      <AppFooter></AppFooter>
+    </div>
+    <div v-else>
+      <SideBar></SideBar>
+      <div class="view">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   import AppHeader from "./components/Header";
   import AppFooter from "./components/Footer";
+  import SideBar from "./components/SideBar";
   export default {
-    components: {AppHeader, AppFooter}
+    props: {
+      login: Boolean
+    },
+    methods: {
+      isLogged() {
+        this.login = !this.login;
+      }
+    },
+    mounted() {
+      this.$root.$on('is-logged', this.isLogged);
+    },
+    components: {AppHeader, AppFooter, SideBar}
   }
 </script>
 
