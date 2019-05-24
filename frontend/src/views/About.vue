f159d12032111be7f3db079448b7dece612edd06
Gustavo Costa <gustavo.neves@estudante.ufla.br>
2019-05-24 19:56:09 -0300

    Criada telas de sobre e contato

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,8 +1,20 @@
-<template>
-    <el-container class="container">
-        <h1>About</h1>
-        <p>Lorem Ipsum</p>
-    </el-container>
+<template lang="pug">
+    .sobre
+        .titulo-pagina INFORMACOES
+        .card-sobre
+            el-row(type="flex")
+                .title.texto PROFESSOR:
+                .texto RAFAEL DURELLI
+            el-row
+                .title.texto FORMACAO:
+                .texto DOUTORADO EM CIENCIAS DA COMPUTACAO E MATEMATICA COMPUTACIONAL. USP, BRASIL(2016).
+            el-row(type="flex")
+                .title.texto SALA:
+            el-row(type="flex")
+                .title.texto TELEFONE:
+            el-row(type="flex")
+                .title.texto EMAIL:
+                .texto RAFAEL.DURELLI@UFLA.BR
 </template>
 
 <script>
@@ -12,9 +24,35 @@ export default {
 </script>
 
 <style lang="scss">
-    .container {
-        background-color: #E4E6E9;
-        height: 75vh;
+    .sobre {
+        width: 80%;
+        margin: 0 auto;
+        padding: 100px 0;
         color: #112A37;
+
+        .card-sobre{
+            border: solid white 2px;
+            border-radius: 10px;
+            padding: 40px 20px;
+        }
+        .texto{
+            font-size: 25px;
+            padding-left: 10px;
+
+            font-weight: lighter;
+        }
+
+        .el-row{
+            padding: 20px;
+        }
+        .title{
+            font-weight: bold;
+        }
+
+        .titulo-pagina{
+            padding-bottom: 30px;
+            font-size: 50px;
+            font-weight: lighter;
+        }
     }
 </style>
--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,20 +1,55 @@
-<template>
-    <el-container class="container">
-        <h1>Contact</h1>
-        <p>Lorem Ipsum</p>
-    </el-container>
+<template lang="pug">
+    .sobre
+        .titulo-pagina CONTATO
+        .card-sobre
+            el-form
+                el-form-item(label="NOME:")
+                    el-input
+                el-form-item(label="EMAIL:")
+                    el-input
+                el-form-item(label="MENSAGEM:")
+                    el-input
+                el-form-item
+                    el-button.botao-enviar ENVIAR
+
 </template>
 
 <script>
-export default {
-    name: "Contact"
-}
+    export default {
+        name: "Contact"
+    }
 </script>
 
 <style lang="scss">
-    .container {
-        background-color: #E4E6E9;
-        height: 75vh;
+    .sobre {
+        width: 30%;
+        margin: 0 auto;
+        padding: 100px 0;
         color: #112A37;
+
+        .card-sobre{
+            border: solid white 2px;
+            border-radius: 10px;
+            padding: 40px 20px;
+        }
+        .texto{
+            font-size: 25px;
+            padding-left: 10px;
+
+            font-weight: lighter;
+        }
+
+        .el-row{
+            padding: 20px;
+        }
+        .title{
+            font-weight: bold;
+        }
+
+        .titulo-pagina{
+            padding-bottom: 30px;
+            font-size: 50px;
+            font-weight: lighter;
+        }
     }
 </style>
