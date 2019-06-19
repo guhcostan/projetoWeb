0ecf4a081f6165ba2d762e3ec85d683ff09a957b
Nathalia Rezende <silvarnath@gmail.com>
2019-06-18 21:03:06 -0300

    ajustando detalhes conforme review

--- frontend/src/views/Dashboard/Alunos.vue
+++ frontend/src/views/Dashboard/Alunos.vue
@@ -2,12 +2,15 @@
     <el-container>
         <Breadcrumb :professor="professor"></Breadcrumb>
         <el-row class="row" type="flex" justify="space-between" align="middle">
-            <el-col class="aluno-head" :span="25">
+            <el-col class="aluno-head" :span="24" :md="5">
                 <h1>Alunos</h1>
             </el-col>
-            <el-col :span="25">
-                <router-link to="/cadastrar_aluno" class="cadastrar-aluno-btn">Cadastrar</router-link>
+            <el-col :span="24" :md="5">
+                <el-button class="cadastrar-aluno-btn" @click="$router.push('/cadastrar_aluno');"> Cadastrar </el-button>
             </el-col>
+        </el-row>
+        <el-row>
+
         </el-row>
     </el-container>
 </template>
@@ -33,14 +36,13 @@ export default {
         height: 70vh;
     }
     .row {
-        padding: 25px 0px;
+        padding: 20px 0px;
         h1 {
                 padding: 60px;
                 font-size: 2rem;
                 font-weight: lighter;
             }
         .cadastrar-aluno-btn {
-            margin-right: 60px;
             text-decoration: none;
             color: #FFFFFF;
             background-color: #63C3A8;
